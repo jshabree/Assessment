86a043e9da8d6b18b656e21fd8b0bd72a0e62114
jshabree <jshabree@emich.edu>
2020-10-21 19:10:22 -0400

    Added bootstrap stylesheet

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Routes from './routes'
 // import Userform from "./components/Userform"
 // import Navbar from "./components/Navbar"
 import './components/styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
   return (
     <div className="App">
