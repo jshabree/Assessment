558f845b6a6c33e39c5c00671075f4499660e685
jshabree <jshabree@emich.edu>
2020-10-21 16:51:04 -0400

    creating first home component

--- src/components/home.js
+++ src/components/home.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react'
 export default class home extends Component {
     render() {
         return (
-            <div style={{textAlign: "center", marginTop: "300px" }}>
-                login successful
+            <div className = 'a' style={{textAlign: "left", marginTop: "250px" }}>
+            <h3 className = 'sidebarHeading'> Super Delicious Cookies </h3>
+            <p>that are also gluten free, vegan & allergy-friendly</p>
+            <button>SHOP NOW</button>
             </div>
         )
     }
