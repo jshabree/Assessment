d82e8783529cf939889987075153b920a629a942
jshabree <jshabree@emich.edu>
2020-10-22 13:00:12 -0400

    first home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
-import { Col, Container } from 'react-bootstrap'
+// import { Col, Row, Container } from 'react-bootstrap'
 
 export default class home extends Component {
     render() {
         return (
-            <div className = 'a' style={{textAlign: "left", marginTop: "250px" }}>
+
+            <div className = 'a' style = {{textAlign: "left", marginTop: "150px" }}>
+
             <h1 className = 'sidedebarHeading'> Super Delicious Cookies </h1>
             <p className = 'midHeading'> that are also gluten free, vegan & allergy-friendly</p>
             <button className = 'lowButton' >Shop Now</button>
+
             </div>
+            
         )
     }
 }
