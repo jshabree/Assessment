0a1cf99a0eda4100b6cb38ebca1a82f46445b5d2
jshabree <jshabree@emich.edu>
2020-11-18 11:19:12 -0500

    writing first mock test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from "react-dom";
 import App from './App';
+import nock from 'nock';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
