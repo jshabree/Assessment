fd47f4ecbd7c6dbfecd9d170818af98bfc11d6d2
jshabree <jshabree@emich.edu>
2020-11-18 11:19:37 -0500

    test for checking service call

--- src/__tests__/posts.spec.js
+++ src/__tests__/posts.spec.js
@@ -1,5 +1,14 @@
 import waitUntil from 'async-wait-until';
-import { shallow } from 'enzyme';
-import { nock } from "nock";
+import { shallow } from 'enzyme'
+import { nock } from 'nock';
 import { React } from "react";
 import Posts from '../components/posts';
+
+describe('<Posts />', () => {
+    beforeAll(() => {
+
+    // prepare nock to respond to a request to posts API
+        nock("https://jsonplaceholder.typicode.com/posts")
+            .get('?_limit=5')
+    });
+});
